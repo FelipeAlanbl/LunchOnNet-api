adddc30310414eb81ccafa1f07945438031b7303
Felipe <dev_back@bluelogic.com.br>
2022-05-14 19:20:30 -0300

    feature: protecting auth routes

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -1,8 +1,32 @@
-function AuthMidleware(req, res, next){
+const jwt = require('jsonwebtoken');
 
-    console.log('Authorization', req.headers.authorization);
+async function AuthMidleware(req, res, next){
 
-    next();
+    try {
+        if(!req.headers.authorization) {
+            return res.status(500).json({message: 'Access unauthorized'});
+        }
+    
+        const [bearer, token] = req.headers.authorization.split(' ');
+    
+        if(bearer !== 'Bearer') {
+            return res.status(500).json({message: 'Token malformatted'});
+        }
+        
+        if(!token) {
+            return res.status(500).json({message: 'Token invalid'});
+        }
+    
+        const tokenIsValid = await jwt.verify(token, process.env.APP_SECRET);
+
+        next();
+
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+
+
+
 }
 
 module.exports = AuthMidleware;
